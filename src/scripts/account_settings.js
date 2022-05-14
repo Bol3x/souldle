7d8c60f6fdd53769063114672f86cafe0c7e5398
Bol3x <carloroleda@gmail.com>
2022-05-14 14:29:02 +0800

    fixed account settings page

---
+++ src/scripts/account_settings.js
@@ -0,0 +1,9 @@
+function openForm() {
+    $("#delete").css("display", "block");
+    $("#login").css("display", "none");
+}
+
+function closeForm() {
+    $("#delete").css("display", "none");
+    $("#login").css("display", "block");
+}
