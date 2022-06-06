a98f99c3cfdf5386024d6dbd4cd8d0cffec2de98
Bol3x <carloroleda@gmail.com>
2022-06-06 22:17:34 +0800

    avatar customization scripts and server post

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = new express();
 mongoose.connect("mongodb://localhost/souldletestdb");
 
+//database schemas
 const User = require('./database/models/User');
 const Item = require('./Database/models/Item');
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//view engine to be used is Embedded JavaScript (EJS)
 app.set('view engine', 'ejs');
 app.set('views', 'view');   //set engine to look for ejs files in view
 
@@ -19,7 +21,12 @@ var server = app.listen(3000, () =>{
     console.log("server is running at port " + port);
 })
 
+//path for static file (html, css, asset) serving
 app.use(express.static(__dirname + '/view/'));
+
+/**
+ * API CALLS
+ */
 
 app.get('/', (req,res) => {
     res.render('index');
@@ -50,14 +57,39 @@ app.get('/customize', async (req, res)=>{
     const user = await User.find({name: "Buranku"}, 'avatar').populate({path: 'avatar.hat'}).populate({path: 'avatar.weapon'}).limit(1);
     //console.log(user[0]);
     avatar = user[0].avatar;
+
+    //query user's weapon collection (item_collection.weapons)
     const weapons = await Item.find({equip_slot: "weapon"});
     //console.log(weapons);
 
+    //query user's headgear collection (item_collection.hats)
     const heads = await Item.find({equip_slot: "head"});
     //console.log(heads);
 
     res.render('customize', {avatar , weapons, heads});
 })
+
+app.post('/customize/save', async(req,res)=>{
+    //item queries
+    //weapon
+    const newWeapon = await Item.findOne({item_name: req.body.weapon});
+    //console.log(newWeapon);
+
+    //head
+    const newHead = await Item.findOne({item_name : req.body.head});
+    //console.log(newHead);
+
+    //user update
+    const user = await User.findOne({name: 'Buranku'});
+
+    user.avatar = {weapon: newWeapon, hat: newHead};
+    //console.log(user);
+    await user.save((err, res) =>{
+        if (err)
+                console.log(err);
+        else    console.log(res);
+    });
+});
 
 app.get('/search', (req,res) =>{
     res.render('search_user');
--- view/scripts/customize.js
+++ view/scripts/customize.js
@@ -1,30 +1,67 @@
 $(document).ready(function () {
-	name = $('.weapon').attr('alt');
-	$("button[name='" + name + "']").addClass('selected');
-	
+	weap_equip = $('.weapon').attr('alt');
+	$("button[name='" + weap_equip + "']").addClass('selected');
+
+	head_equip = $('.head').attr('alt');
+	$("button[name='" + head_equip + "']").addClass('selected');
+
+	//weapon customize UI
 	$('#weaponbox').on('click', '.item', function () {
+		//weapon data
 		type = $(this).attr('id');
 		img_src = $(this).children("img").attr("src");
+		item_name = $(this).attr('name');
 
+		//show selected weapon
 		$('.weaponslot').removeClass('selected');
 		$(this).addClass('selected');
+
+		//append attributes of chosen weapon
 		$('.weapon').attr('id', type);
 		$('.weapon').attr('src', img_src);
+		$('.weapon').attr('alt', item_name);
 	});
 
+	//head customize UI
 	$('#hatbox').on('click', '.item', function () {
+		//hat data
 		type = $(this).attr('id');
 		img_src = $(this).children("img").attr("src");
+		item_name = $(this).attr('name');
 
+		//show selected head
 		$('.headslot').removeClass('selected');
         $(this).addClass('selected');
+
+		//append attributes of chosen head
 		if ($(this).attr('id') === 'unequip_hat'){
 			$('.head').removeAttr('src');
 			$('.head').removeAttr('id');
+			$('.head').removeAttr('alt');
 		}
 		else{
 			$('.head').attr('id', type);
 			$('.head').attr('src', img_src);
+			$('.head').attr('alt', item_name);
 		}
 	});
+
+	$('#save').click(function (){
+		$.post("/customize/save", saveChanges(),
+			function (data, textStatus) {
+				console.log(data);
+			}
+		);
+	})
+
 });
+
+//returns a JSON file containing the name of the items to be saved
+saveChanges = () => {
+	let weaponName = $('.weapon').attr('alt');
+	let hatName = $('.head').attr('alt');
+	if (hatName === undefined)
+		return ({weapon: weaponName, head: null});
+	else
+		return ({weapon: weaponName, head: hatName});
+}
