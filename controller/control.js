9f19acbc0a0f70b68595956d210e0ce25a7c51f4
Shaneuy <shane_owen_uy@dlsu.edu.ph>
2022-06-30 00:27:21 +0800

    Session handling things

--- controller/control.js
+++ controller/control.js
@@ -3,8 +3,10 @@ const Item = require('../database/models/Item.js');
 
 const controller = {
 	getIndex: function(req,res){
-		// if session exists, res.render('home');
-		res.render('index');
+		if(req.session.name != null)
+			res.render('home');
+		else
+			res.render('index');
 	},
 
 	getPlay: function (req,res){
@@ -12,15 +14,37 @@ const controller = {
 	},
 	
 	getHome: function (req,res){
-		res.render('home');
+		if(req.session.name != null)
+			res.render('home');
+		else
+			res.render('index');
 	},
-
+	
 	getLogin : function(req, res){
 		res.render('login');
 	},
 	
 	getAccountSettings : function(req, res){
-		res.render('account_settings');
+		if(req.session.name != null)
+			res.render('account_settings');
+		else
+			res.render('index');
+		
+	},
+			
+	getProfile: async function(req,res){
+		if(req.session.name != null) {
+			const name = req.session.name;
+			const uid = req.session.user;
+		
+			const user = await User.findOne({name, uid}, "avatar statistics").populate({path: 'avatar.hat'}).populate({path: 'avatar.weapon'});
+			//console.log(user);
+			const avatar = user.avatar;
+			const stats = user.statistics;
+			res.render('profile', {name, avatar, stats});
+		}
+		else
+			res.render('index');
 	}
 }
 
--- controller/customize.js
+++ controller/customize.js
@@ -4,21 +4,26 @@ const Item = require('../database/models/Item.js');
 const customize = {
     getCustomize: async function (req, res){
 		//TODO: Session Handling
-		const user = await User.findOne({name: req.session.name}, 'avatar item_collection')
-		.populate({path: 'avatar.hat'}).populate({path: 'avatar.weapon'})
-		.populate({path: 'item_collection.weapons'}).populate({path: 'item_collection.hats'});
-		//console.log(user);
-		avatar = user.avatar;
+		if(req.session.name != null) {
+			const user = await User.findOne({name: req.session.name}, 'avatar item_collection')
+			.populate({path: 'avatar.hat'}).populate({path: 'avatar.weapon'})
+			.populate({path: 'item_collection.weapons'}).populate({path: 'item_collection.hats'});
+			//console.log(user);
+			avatar = user.avatar;
 
-		//query user's weapon collection (item_collection.weapons)
-		const weapons = user.item_collection.weapons;
-		//console.log(weapons);
+			//query user's weapon collection (item_collection.weapons)
+			const weapons = user.item_collection.weapons;
+			//console.log(weapons);
 
-		//query user's headgear collection (item_collection.hats)
-		const heads = await user.item_collection.hats;
-		//console.log(heads);
+			//query user's headgear collection (item_collection.hats)
+			const heads = await user.item_collection.hats;
+			//console.log(heads);
 
-		res.render('customize', {avatar , weapons, heads});
+			res.render('customize', {avatar , weapons, heads});
+		}
+
+		else
+			res.render('index');
 	},
 
 	postCustomizeSave: async function(req,res){
