5351b8b668477b688ab14144a96260a57b30db98
JPMPDev <95627276+JPMPDev@users.noreply.github.com>
2022-07-01 22:54:47 +0800

    Update Answer.js

--- database/models/Answer.js
+++ database/models/Answer.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const AnswerSchema = new Schema({
 	answer : {type: String, required: true, max: 5},
     from : {type: String, max: 20},
-	date : {type: String, required: true},
 	hour : {type: Number, required: true}
 });
 
