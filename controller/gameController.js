688e22eb619e885faf25c226cf8c3cf194458e03
JPMPDev <95627276+JPMPDev@users.noreply.github.com>
2022-07-01 23:00:28 +0800

    Profound Happiness
    //TODO: fix redirects, render partials

--- controller/gameController.js
+++ controller/gameController.js
@@ -1,35 +1,90 @@
 const User = require('../database/models/User.js');
+const Answer = require('../database/models/Answer.js');
 const game = {
-	gamewin: async function(req,res){
-		const user = await User.findOne({name: 'Buranku'}, "souls statistics"); //unsure what to place at the second part
-		//increment souls by 5 because they guessed all of it correctly
-		user.souls += 5;
-		//update stats to accomodate loss
-		//increment winstreak by 1
-		user.statistics.win_streak += 1;
-		var ws = user.statistics.win_streak;
-		//if win_streak > max_streak, set max_streak to win_streak
-		if (ws > user.statistics.max_streak)
-		user.statistics.max_streak = ws;
-		var ms = user.statistics.max_streak;
-		//increment num_wins by 1
-		user.statistics.num_wins += 1;
-		var nwins = user.statistics.num_wins;
-		//increment num_games by 1
-		user.statistics.num_games += 1;
-		var gms = user.statistics.num_games;
-		res.status(200).send({message: "You Win!", souls: user.souls, winst: ws, max: ms, wins: nwins, ngms: gms });
-	},
-	gameloss: async function(req,res){
-		const user = await User.findOne({name: 'Buranku'}, "souls statistics"); //unsure what to place at the second part
-		//update souls based on all correct guesses
+	uploadAnswer: async function(req,res){
+		/*
+            when submitting forms using HTTP POST method
+            the values in the input fields are stored in `req.body` object
+            each <input> element is identified using its `name` attribute
+            Example: the value entered in <input type="text" name="fName">
+            can be retrieved using `req.body.fName`
+        */
+		var datetoday = new Date();
+		var currentHour = datetoday.getHours();
 		
-		//update stats to accomodate loss
-			//update win_streak to 0
-			user.statistics.win_streak - 0;
-			//no change to max_streak and num_wins
-			//increment num_games by 1
-			user.statistics.num_games += 1;
+        var tfrom = req.session.name;
+        var tanswer = req.body.answer;
+        var thour = currentHour;
+
+        const ans = {
+            answer: tanswer,
+            from: tfrom,
+            hour: thour
+        }
+		try{
+			var answ = new Answer(ans);
+			answ.save();
+			req.flash('success_msg', 'Answer submitted');
+			res.redirect('/home');
+		}catch(err){
+			console.log(err);
+			req.flash('error_msg', 'Could not submit answer, please try again.');
+			res.redirect('/play');
+		}
+
+	},
+	getPlayed: async function(req,res){
+		var datetoday = new Date();
+		var currentHour = datetoday.getHours();
+		const played = await Answer.findOne({from: req.session.name, hour : currentHour});
+		if (played == null)
+			//we are good
+			res.send("OK");
+		else
+			//they have already played
+			res.send("BAD");
+	},
+	getAnswer: async function(req,res){
+		const answer = await Answer.findOne(req.query); //tentative name of automated admin that will give answers
+		if (answer == null)
+			res.sendStatus(404);
+		else
+			res.send({message : answer.answer});
+	}, 
+	getGuess: async function(req,res){
+		const answer = await Answer.findOne(req.query);
+		if (answer == null)
+			res.sendStatus(404);
+		else
+			res.send({message : answer.answer});
+	},
+	gamewin : async function(req, res){
+		var myquery = { name: req.session.name};
+		const user = await User.findOne({name: req.session.name});
+		var incvalues = { $inc: {souls: 5, "statistics.win_streak": 1, "statistics.num_wins": 1, "statistics.num_games": 1 }};
+		var posinc = {$inc: {"statistics.max_streak": 1}};
+		User.updateOne(myquery, incvalues, function(err, res) {
+			if (err) throw err;
+			console.log("values updated");
+		});
+		if (user.statistics.win_streak > user.statistics.max_streak)
+		User.updateOne(myquery, posinc, function(err, res) {
+			if (err) throw err;
+			console.log("values updated");
+		});
+	},
+	
+	gameloss: async function(req,res){
+		var myquery = { name: req.session.name };
+		console.log(req.session.name);
+		var souladd = req.body.sgain;
+		console.log(souladd);
+		const user = await User.findOne({name: req.session.name});
+		var incvalues = { $inc: {souls: souladd, "statistics.num_games": 1 }};
+		User.updateOne(myquery, incvalues, function(err, res) {
+			if (err) throw err;
+			console.log("values updated");
+		});
 	}
 }
 
